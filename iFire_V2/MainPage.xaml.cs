81f40d3e41440f357f12f870f07547039c9f65da
Ben Johnson <jimbixeren@gmail.com>
2021-02-07 11:47:00 +0100

    Add project files.

---
+++ iFire_V2/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace iFire_V2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var assembly = typeof(MainPage);
+
+
+            //ImageSource.FromResource("iFire_V2.Assets.Images.iFire.png", assembly);
+            ImageSource.FromResource("iFire_V2.Assets.Images.plane.png", assembly);
+
+
+
+        }
+    }
+}
